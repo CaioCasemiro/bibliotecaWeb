21bfe1a10442f90b320c9f016d35185de19629cc
Kaio Mourato <kaiomouratodm@hotmail.com>
2025-03-10 10:35:25 -0300

    Problemas de conexão resolvidos

--- src/main/java/br/com/bibliotecaWeb/BibliotecaWebApplication.java
+++ src/main/java/br/com/bibliotecaWeb/BibliotecaWebApplication.java
@@ -2,12 +2,17 @@ package br.com.bibliotecaWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.bibliotecaWeb")
+@EntityScan(basePackages = "br.com.bibliotecaWeb.models")
 public class BibliotecaWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaWebApplication.class, args);
+	
 	}
 
 }
---
+++ src/main/java/br/com/bibliotecaWeb/models/Livro.java
@@ -0,0 +1,72 @@
+package br.com.bibliotecaWeb.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros") // Define o nome da tabela no banco de dados
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String titulo;
+
+    @Column(nullable = false, length = 100)
+    private String autor;
+
+    @Column(nullable = false)
+    private int anoPublicacao;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo(String string) {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
